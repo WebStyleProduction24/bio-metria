e8fb8dddcbde3a20fbb660ed662992b51b8fa2c1
Jack36VRN <jack3@JACK36-LAPTOP>
2019-05-20 19:01:52 +0300

    Update 2019/05/20 19:01 +3GMT

--- socials_float.php
+++ socials_float.php
@@ -1,5 +1,5 @@
   <div class="social"> 
-  	<a class="social__link" href="https://web.telegram.org/#/im?p=74952054700"><i class="fab fa-telegram-plane"></i></a>
+  	<a class="social__link" href="https://t.me/74952054700"><i class="fab fa-telegram-plane"></i></a>
   	<a class="social__link" href="https://wa.me/74952054700"><i class="fab fa-whatsapp"></i></a>
   	<a class="social__link" href="skype:killjane"><i class="fab fa-skype"></i></a>
   </div> 
