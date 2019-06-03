dca06e426918ab670414d7851950a92b84c5d293
Jack36VRN <jack3@JACK36-LAPTOP>
2019-06-03 18:14:05 +0300

    Update archive-product.php
    Добавлена возможность отображения контента страницы "каталог"

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -45,33 +45,32 @@ get_header( 'shop' );
 		 * @hooked added category_before_list - 50
 		 */
 		do_action( 'woocommerce_before_shop_loop' );
-		if ( is_shop()){ ?>
-			<?php global $post; the_content($post->ID); ?>
-			<ul>
-				<?php wp_list_categories( $args = array(
-					'taxonomy' => 'product_cat',
-					'depth' => 1,
-					'exclude' => '15',
-					'title_li' => 'Категории:'
-				) ); ?>
-			</ul>
-			<?php		
+
+		if ( is_shop()){
+
+			do_action( 'woocommerce_archive_description' );
+
+			?> <ul> <?php 
+			wp_list_categories( $args = array(
+				'taxonomy' => 'product_cat',
+				'depth' => 1,
+				'exclude' => '15',
+				'title_li' => 'Категории:'
+			) );
+			?> </ul> <?php		
 
 		} else if (is_product_taxonomy()) {
 
 			category_after_list();
-			?>
 
-			<ul>
+			?> <ul> <?php	
+			wp_list_categories( $args = array(
+				'taxonomy' => 'product_cat',
+				'child_of' => get_queried_object()->term_id,
+				'title_li' => 'Подкатегории:'
+			) );
 
-				<?php	
-				wp_list_categories( $args = array(
-					'taxonomy' => 'product_cat',
-					'child_of' => get_queried_object()->term_id,
-					'title_li' => 'Подкатегории:'
-				) ); ?>
-			</ul>
-			<?php
+			?> </ul> <?php
 
 		} else {
 
@@ -81,33 +80,34 @@ get_header( 'shop' );
 				while ( have_posts() ) {
 					the_post();
 
+					/**
+					* Hook: woocommerce_shop_loop.
+					*
+					* @hooked WC_Structured_Data::generate_product_data() - 10
+					*/
+					do_action( 'woocommerce_shop_loop' );
+
+					wc_get_template_part( 'content', 'product' );
+				}
+
+			}
+
+			// woocommerce_product_loop_end();
+
+			/**
+			* Hook: woocommerce_after_shop_loop.
+			*
+			* @hooked woocommerce_pagination - 10
+			*/
+			do_action( 'woocommerce_after_shop_loop' );
+		}
+	} else {
 	/**
-	* Hook: woocommerce_shop_loop.
+	* Hook: woocommerce_no_products_found.
 	*
-	* @hooked WC_Structured_Data::generate_product_data() - 10
+	* @hooked wc_no_products_found - 10
 	*/
-	do_action( 'woocommerce_shop_loop' );
-
-	wc_get_template_part( 'content', 'product' );
-}
-}
-
-// woocommerce_product_loop_end();
-
-/**
-* Hook: woocommerce_after_shop_loop.
-*
-* @hooked woocommerce_pagination - 10
-*/
-do_action( 'woocommerce_after_shop_loop' );
-}
-} else {
-/**
-* Hook: woocommerce_no_products_found.
-*
-* @hooked wc_no_products_found - 10
-*/
-do_action( 'woocommerce_no_products_found' );
+	do_action( 'woocommerce_no_products_found' );
 }
 
 
