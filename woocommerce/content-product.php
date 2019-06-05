3c3b63bf3266a17881aa354dae9eadc6e76debe9
Jack36VRN <jack3@JACK36-LAPTOP>
2019-06-05 19:58:45 +0300

    Update content-product.php
    Добавлена ссылка на изображение продукта в каталоге

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -36,7 +36,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	 *
 	 * @hooked woocommerce_template_loop_product_link_open - 10
 	 */
-	// do_action( 'woocommerce_before_shop_loop_item' );
+	do_action( 'woocommerce_before_shop_loop_item' );
 
 	/**
 	 * Hook: woocommerce_before_shop_loop_item_title.
@@ -46,6 +46,14 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	 * @hooked added woocommerce_template_loop_product_image - 10
 	 */
 	do_action( 'woocommerce_before_shop_loop_item_title' );
+
+	/**
+	 * Hook: woocommerce_after_shop_loop_item.
+	 *
+	 * @hooked woocommerce_template_loop_product_link_close - 5
+	 * @hooked woocommerce_template_loop_add_to_cart - 10
+	 */
+	do_action( 'woocommerce_after_shop_loop_item' );
 
 	/**
 	 * Hook: woocommerce_shop_loop_item_title.
@@ -65,14 +73,6 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	 * @hooked added free_consultation - 30
 	 */
 	do_action( 'woocommerce_after_shop_loop_item_title' );
-
-	/**
-	 * Hook: woocommerce_after_shop_loop_item.
-	 *
-	 * @hooked woocommerce_template_loop_product_link_close - 5
-	 * @hooked woocommerce_template_loop_add_to_cart - 10
-	 */
-	// do_action( 'woocommerce_after_shop_loop_item' );
 }
 ?>
 </div>
