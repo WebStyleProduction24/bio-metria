bbc0893d5445486b36004c44e76c5339c596c8d1
Jack36VRN <jack3@JACK36-LAPTOP>
2019-06-07 16:30:01 +0300

    Update 2019/06/07 16:29 +3GMT
    Предварительная версия отображения контента страницы и записи.

--- page.php
+++ page.php
@@ -5,7 +5,11 @@
 
 	<div class="container">
 
-<h1><?php the_title(); ?></h1>
+		<div class="post">
+			<?php echo get_the_post_thumbnail( null, 'full', array('class' => 'img_post_thumbnail') ); ?>
+			<h1 class="product-desc__title"><?php the_title(); ?></h1>
+			<?php the_content(); ?>
+		</div>
 	</div>
 
 </section>
