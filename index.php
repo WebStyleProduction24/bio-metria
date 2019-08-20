b8aa7756585bd34a7d3746988e2278b61a30d622
Jack36VRN <jack3@JACK36-LAPTOP>
2019-08-20 15:11:20 +0300

    Update 2019/08/20 15:11 +3GMT
    Завершена страница блога и его отдельных страниц

--- index.php
+++ index.php
@@ -14,15 +14,15 @@
 
 		<?php if( have_posts() ){ while( have_posts() ){ the_post(); ?>
 
-			<div class="col-10 blog">
+			<div class="col-lg-10 blog">
 				<div class="row">
-					<div class="col-4"><a href="<?php the_permalink(); ?>"><img src="<?php echo get_the_post_thumbnail_url(); ?>" alt=""></a></div>
-					<div class="col-8">
+					<div class="col-sm-4 px-0 px-sm-3"><a href="<?php the_permalink(); ?>"><img src="<?php echo get_the_post_thumbnail_url(); ?>" alt=""></a></div>
+					<div class="col-sm-8">
 						<div class="row align-items-end h-100">
 							<p class="category-list__name"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
 							<?php the_excerpt(); ?>
 							<div class="row justify-content-end w-100">
-								<div class="col-4">
+								<div class="col-lg-4 col-md-7">
 									<a href="<?php the_permalink(); ?>" class="articles__link">Читать статью</a>
 								</div>
 							</div>
--- single.php
+++ single.php
@@ -6,7 +6,7 @@
 		
 		<?php if( have_posts() ){ while( have_posts() ){ the_post(); ?>
 
-					<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" style="float: left;">
+					<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" style="float: left;" class="single">
 					<h1 class="product-desc__title title-our-blog single"><?php the_title(); ?></h1>
 					<?php the_content(); ?>
 
