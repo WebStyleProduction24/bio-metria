598acb61b1fad0de199b31868baec49134c0426d
Jack36VRN <36456429+Jack36VRN@users.noreply.github.com>
2020-01-30 20:04:50 +0300

    Update 2020/01/30 20:03 +3GMT
    Тема совместима с WooCommerce версии 3.9.1

--- woocommerce/single-product/tabs/tabs.php
+++ woocommerce/single-product/tabs/tabs.php
@@ -10,9 +10,9 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	https://docs.woocommerce.com/document/template-structure/
+ * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 2.4.0
+ * @version 3.8.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -25,33 +25,40 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Each tab is an array containing title, callback and priority.
  * @see woocommerce_default_product_tabs()
  */
-$tabs = apply_filters( 'woocommerce_product_tabs', array() );
 
-if ( ! empty( $tabs ) ) : ?>
+$product_tabs = apply_filters( 'woocommerce_product_tabs', array() );
+
+if ( ! empty( $product_tabs ) ) : ?>
 
 	<div class="product-info col-lg-12">
 		<ul class="product-tab">
-			<?php foreach ( $tabs as $key => $tab ) : ?>
+			<?php foreach ( $product_tabs as $key => $product_tab ) : ?>
 				<?php 
 				if (esc_attr( $key ) == 'description') {$data_id = '1'; $active = ' product-tab__item-active';}
 				else if (esc_attr( $key ) == 'video') {$data_id = '2'; $active = '';}
 				else if (esc_attr( $key ) == 'programm') {$data_id = '3'; $active = '';}
 				?>
 				<li data-id="<?php echo $data_id; ?>" class="product-tab__item<?php echo $active; ?>">
-					<?php echo apply_filters( 'woocommerce_product_' . $key . '_tab_title', esc_html( $tab['title'] ), $key ); ?>
+						<?php echo wp_kses_post( apply_filters( 'woocommerce_product_' . $key . '_tab_title', $product_tab['title'], $key ) ); ?>
 				</li>
 			<?php endforeach; ?>
 		</ul>
-		<?php foreach ( $tabs as $key => $tab ) : ?>
+		<?php foreach ( $product_tabs as $key => $product_tab ) : ?>
 			<?php 
 			if (esc_attr( $key ) == 'description') {$data_id = '1'; $style = 'style="display:block"';}
 			else if (esc_attr( $key ) == 'video') {$data_id = '2'; $style = 'style="display:none"';}
 			else if (esc_attr( $key ) == 'programm') {$data_id = '3'; $style = 'style="display:none"';}
 			?>
 			<div data-id="<?php echo $data_id; ?>" class="product-tab__content" <?php echo $style; ?>>
-				<?php if ( isset( $tab['callback'] ) ) { call_user_func( $tab['callback'], $key, $tab ); } ?>
+				<?php
+				if ( isset( $product_tab['callback'] ) ) {
+					call_user_func( $product_tab['callback'], $key, $product_tab );
+				}
+				?>
 			</div>
 		<?php endforeach; ?>
+		<?php do_action( 'woocommerce_product_after_tabs' ); ?>
+
 	</div>
 
 <?php endif; ?>
