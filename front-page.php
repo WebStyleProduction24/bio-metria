f5909de84ac53cc458abdd092a1cb23fca579594
Jack36VRN <36456429+Jack36VRN@users.noreply.github.com>
2020-09-16 12:21:16 +0300

    Update front-page.php
    Возвращены Предложения под ключ

--- front-page.php
+++ front-page.php
@@ -4,26 +4,17 @@
 
 <section class="main">
   <div class="container">
-   <!--  <div class="about row">
-      <div class="col-lg-6">
-        <span class="title">О компании</span>
-        <p class="about__text">
-          <br>Если ваше внимание привлекла эта страница, значит вы точно понимаете, что уровень личной или коммерческой безопасности, учета и анализа приходящей информации должен переходить на более высокую ступень.  Концепция проверки биометрической идентичности не нова:  она существует уже тысячи лет. Даже то, что мы думаем об этом сегодня, имеет более чем десятилетнюю историю.</br>  
-          Биометрия сегодня означает уникальные для каждого человека физиологические характеристики, считываемые специально разработанными приборами для определения или проверки личности.  Опираясь на свой многолетний опыт в технологиях распознавания и идентификации мы постоянно расширяем ассортимент продукции лучших производителей, представляя только самые высокоточные биометрические продукты
-        </p>
-        <a class="btn text-right" href="/catalog">Подробнее о компании</a>
-      </div>
-      <div class="col-lg-6">
-        <img class="about__img" src="<?php// echo get_template_directory_uri(); ?>/img/about-img.jpg" alt="">
-      </div>
-    </div> -->
-    <!-- <div class="proposition row">
+
+<!-- Блок Предложения под ключ -->
+
+
+    <div class="proposition row">
       <div class="title-line col-lg-12">
         <span class="title title_bg">ПРЕДЛОЖЕНИЯ ПОД КЛЮЧ</span>
       </div>
       <div class="col-lg-3 col-md-6">
         <div class="proposition__item">
-          <img src="<?php // echo get_template_directory_uri(); ?>/img/proposition-img.png" alt="" class="proposition__img">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/proposition-img.png" alt="" class="proposition__img">
           <span class="proposition__title">Биометрическая подпись</span>
           <p class="proposition__text">Используйте данное программное обеспечение для удостоверения документов рукописной подписью в электронном виде. </p>
           <a class="btn" href="/catalog">Перейти в каталог</a>
@@ -31,7 +22,7 @@
       </div>
       <div class="col-lg-3 col-md-6">
         <div class="proposition__item">
-          <img src="<?php // echo get_template_directory_uri(); ?>/img/proposition-img.png" alt="" class="proposition__img">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/proposition-img.png" alt="" class="proposition__img">
           <span class="proposition__title">Биометрическая подпись</span>
           <p class="proposition__text">Используйте данное программное обеспечение для удостоверения документов рукописной подписью в электронном виде. </p>
           <a class="btn" href="/catalog">Перейти в каталог</a>
@@ -39,7 +30,7 @@
       </div>
       <div class="col-lg-3 col-md-6">
         <div class="proposition__item">
-          <img src="<?php // echo get_template_directory_uri(); ?>/img/proposition-img.png" alt="" class="proposition__img">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/proposition-img.png" alt="" class="proposition__img">
           <span class="proposition__title">Биометрическая подпись</span>
           <p class="proposition__text">Используйте данное программное обеспечение для удостоверения документов рукописной подписью в электронном виде. </p>
           <a class="btn" href="/catalog">Перейти в каталог</a>
@@ -47,25 +38,14 @@
       </div> 
       <div class="col-lg-3 col-md-6">
         <div class="proposition__item">
-          <img src="<?php // echo get_template_directory_uri(); ?>/img/proposition-img.png" alt="" class="proposition__img">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/proposition-img.png" alt="" class="proposition__img">
           <span class="proposition__title">Биометрическая подпись</span>
           <p class="proposition__text">Используйте данное программное обеспечение для удостоверения документов рукописной подписью в электронном виде. </p>
           <a class="btn" href="/catalog">Перейти в каталог</a>
         </div>
       </div>
-    </div> -->
-<!--     <div class="partners row">
-      <div class="title-line col-lg-12">
-        <span class="title">Наши партнеры</span>
-      </div> 
-      <div class="partners__list col-lg-12">
-        <img class="partners__img" src="<?php // echo get_template_directory_uri(); ?>/img/partner-1.png" alt="">
-        <img class="partners__img" src="<?php // echo get_template_directory_uri(); ?>/img/partner-2.png" alt="">
-        <img class="partners__img" src="<?php // echo get_template_directory_uri(); ?>/img/partner-3.png" alt="">
-        <img class="partners__img" src="<?php // echo get_template_directory_uri(); ?>/img/partner-4.png" alt="">
-        <img class="partners__img" src="<?php // echo get_template_directory_uri(); ?>/img/partner-5.png" alt="">
-      </div>
-    </div> -->
+    </div>
+
     <div class="articles row mb-5">      
       <div class="title-line col-lg-12">
         <span class="title">Статьи</span>
